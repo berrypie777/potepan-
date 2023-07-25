299d6524cce2f66dc3b3c8391e54c983a3b0326f
berrypie777 <ritacontact888@gmail.com>
2023-07-25 12:13:33 +0000

    controllerの記述を修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,4 +1,7 @@
 module Potepan
-  class ProductsController < Applicationcontroller
+  class ProductsController < ApplicationController
+    def show
+      @product = Spree::Product.find(params[:id])
+    end
   end
 end
