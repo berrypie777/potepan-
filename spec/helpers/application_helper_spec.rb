88416f437a97be7c3d29a8ffcbb0e880f48ffee7
Kenta Koana <administrator@My-Macbook-Air.local>
2023-07-31 21:26:24 +0900

    ページタイトルのテスト処理を変更

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe "#full_title" do
+    let(:product) { double("Product", name: "Single Product") }
+
     it "returns base title when page_title is blank" do
       expect(full_title("")).to eq "BIGBAG Store"
     end
 
     it "returns the correct full title when page_title is provided" do
-      page_title = "Single Product"
+      page_title = product.name
       expect(full_title(page_title)).to eq "#{page_title} - BIGBAG Store"
     end
   end
