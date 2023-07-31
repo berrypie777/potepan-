86837659c4d9e661e750030a999fb4a35e92fd65
Kenta Koana <administrator@My-Macbook-Air.local>
2023-07-31 19:34:00 +0900

    rspecのためにrails-controller-testingをインストール

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
+require 'spree/testing_support/factories'
 
 RSpec.describe "Potepan::ProductsController", type: :request do
-  describe "GET /potepan/products" do
-    it "works! (now write some real specs)" do
-      get potepan_products_index_path
-      expect(response).to have_http_status(200)
+  describe "potepan/products/show" do
+    let!(:product) { create(:product) }
+
+    before do
+      get "/potepan/products/#{product.id}"
     end
+
+    it "HTTP Status Code must be 200" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "show template is successfully rendered" do
+      expect(response).to render_template :show
+    end
+
+    it "@product is successfully assigned" do
+      expect(assigns(:product)).to eq product
+    end
+
   end
 end
