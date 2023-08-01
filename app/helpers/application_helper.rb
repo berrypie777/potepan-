952f62eedd1051a8c50676a523f8a9ba9b931bd3
Kenta Koana <administrator@My-Macbook-Air.local>
2023-08-01 16:45:49 +0900

    rubocopのエラーに対処

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
-  
   BASE_TITLE = "BIGBAG Store".freeze
   def full_title(page_title)
     if page_title.nil?
-      raise RuntimeError, "Page title is nil"
+      raise "Page title is nil"
     elsif page_title.blank?
       BASE_TITLE
     else
