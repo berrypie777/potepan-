2108643145d77b3e48adffdf21b5d2af582e06a1
Kenta Koana <administrator@My-Macbook-Air.local>
2023-08-02 15:29:15 +0900

    レビュアーの指摘を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,6 @@ module ApplicationHelper
   BASE_TITLE = "BIGBAG Store".freeze
   def full_title(page_title)
     if page_title.nil?
-      raise "Page title is nil"
-    elsif page_title.blank?
       BASE_TITLE
     else
       "#{page_title} - #{BASE_TITLE}"
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,18 +4,14 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe "#full_title" do
     let(:product) { double("Product", name: "Single Product") }
 
-    it "returns base title when page_title is blank" do
-      expect(full_title("")).to eq "BIGBAG Store"
+    it "returns base title when page_title is nil" do
+      page_title = nil
+      expect(full_title(page_title)).to eq "BIGBAG Store"
     end
 
     it "returns the correct full title when page_title is provided" do
       page_title = product.name
       expect(full_title(page_title)).to eq "#{page_title} - BIGBAG Store"
-    end
-
-    it "returns error message when product name is nil" do
-      page_title = nil
-      expect{ full_title(page_title) }.to raise_error(RuntimeError, "Page title is nil")
     end
   end
 end
