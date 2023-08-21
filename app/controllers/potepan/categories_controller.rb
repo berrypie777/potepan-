dad89c4353314bf2dcedf134e90d3ba4144a3281
Kenta Koana <administrator@My-Macbook-Air.local>
2023-08-22 06:07:29 +0900

    categories_controllerを記述

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,4 +1,9 @@
 module Potepan
   class CategoriesController < ApplicationController
+    def show
+      @taxonomies = Spree::Taxonomy.includes(:root)
+      @taxon = Spree::Taxon.find(params[:id])
+      @products = @taxon.products.includes(master: [:default_price, :images])
+    end
   end
 end
