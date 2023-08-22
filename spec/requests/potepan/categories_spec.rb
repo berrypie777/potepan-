d0437d9dd26c3e50a3742853bcd1be8b8e39dd5e
Kenta Koana <administrator@My-Macbook-Air.local>
2023-08-23 00:37:07 +0900

    リクエストスペックを実装

--- spec/requests/potepan/categories_spec.rb
+++ spec/requests/potepan/categories_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Potepan::CategoriesController ", type: :request do
-  describe "potepan/categories/:taxon_id" do
-    
+  describe "GET categories/show" do
+      let(:taxonomy) { create(:taxonomy) }
+      let(:taxon) { create(:taxon, parent_id: taxonomy.root) }
+      let!(:product) { create(:product, taxons: [taxon]) }
+      let(:image) { create(:image) }
+
+      before do
+        product.images << image
+        get potepan_category_path(taxon.id)
+      end
+
+      it "HTTP Status Code must be 200" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "response body contains taxonomy and taxon name" do
+        expect(response.body).to include(taxonomy.name)
+        expect(response.body).to include(taxon.name)
+      end
+
+      it "response body contains product name" do
+        expect(response.body).to include(product.name) 
+      end
+
+      it "response body contains product price" do
+        expect(response.body).to include(product.display_price.to_s)
+      end
   end
 end
